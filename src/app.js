030e28c01ce37bfebcb18b5282e684acdae7ecdc
Can Yılmaz <wwcnylmz@gmail.com>
2021-10-19 00:01:54 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,9 @@
+let konutKredileri = ["Konut Kredisi","Taşıt Kredisi","İhtiyaç Kredisi","Özel Kredi"]
+
+console.log("<ul>")
+
+for(let i = 0;i <konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+
+console.log("</ul>")
